725711c034c308b8b7e3a5786313f3bdce98139f
MoududAhammed <moududahammed0@gmail.com>
2022-02-13 17:22:01 +0600

    bank project input handle almost done

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,8 @@
+function getCurrentBalance(){
+    const balance = document.getElementById('total-balance');
+    const previousTotalAmount = parseFloat(balance.innerText);
+    return previousTotalAmount;
+}
 // get input and make as a float vlue
 function getInput(inputId){
     const inputfield = document.getElementById(inputId);
@@ -7,31 +12,49 @@ function getInput(inputId){
 }
 // add amount with previous total amount
 function addAmount(totalId,newAmount){
-    const total = document.getElementById(totalId);
-    const previousTotal = parseFloat(total.innerText);
-    const currentTotal = previousTotal + newAmount;
-    total.innerText = currentTotal;  
+    if(newAmount>0){
+        const total = document.getElementById(totalId);
+        const previousTotal = parseFloat(total.innerText);
+        const currentTotal = previousTotal + newAmount;
+        total.innerText = currentTotal;
+    }
+      
 }
+
 //cooking total balalnce
-function finalBalance(balalnceId,newAmount){
-    const balalnce = document.getElementById(balalnceId);
-    const previousTotalAmount =parseFloat(balalnce.innerText);
-    const finalValue = previousTotalAmount + newAmount;
-    balalnce.innerText = finalValue;
+function finalBalance(newAmount,isTrue){
+    if(newAmount>0){
+        const balalnce = document.getElementById('total-balance');
+        // const previousTotalAmount =parseFloat(balalnce.innerText);
+        const previousTotalAmount = getCurrentBalance();
+        debugger;
+            if(isTrue==true){
+                const finalValue = previousTotalAmount + newAmount;
+                balalnce.innerText = finalValue;
+            }
+            else{
+                const finalValue = previousTotalAmount - newAmount;
+                balalnce.innerText = finalValue;
+            }
+    }     
 }
 // deposit button click action
 document.getElementById('deposit-button').addEventListener('click',function(){
     const inputAmount = getInput('deposit-input');
     addAmount('deposit-total',inputAmount);
     // add deposit balance with final balance
-    finalBalance('total-balance',inputAmount);
-    
+    finalBalance(inputAmount,true);   
 })
+
 // widthdraw button click action
 document.getElementById('withdraw-button').addEventListener('click',function(){
     const inputAmount = getInput('withdraw-input');
-    addAmount('withdraw-total',inputAmount);
-    // final balance after withdraw
-    finalBalance('total-balance',-inputAmount);
+    const totalAmount  = getCurrentBalance();
+
+    if(totalAmount >= inputAmount){
+        addAmount('withdraw-total',inputAmount);
+        // final balance after withdraw
+        finalBalance(inputAmount,false);
+     } 
 })
 
